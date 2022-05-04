15350119a776705b7d37adb3e115b44b0fde893c
Dmitry G. Anderson <dandgerson@gmail.com>
2022-05-04 04:47:31 +0300

    add mui styling

--- src/BookList/BookList.tsx
+++ src/BookList/BookList.tsx
@@ -1,3 +1,7 @@
+import { Button, Card, CardActionArea, CardActions, CardContent, Grid, Typography } from "@mui/material"
+import { makeStyles } from '@mui/styles'
+import { Link } from "react-router-dom"
+
 export type BookType = {
   name: string
   id: number
@@ -9,6 +13,8 @@ const BookList = ({ books, isLoading, isError }: {
   isLoading?: boolean
   isError?: boolean
 }) => {
+  const classes = useStyles()
+  console.log(classes)
 
   if (isLoading) {
     return <p>Loading...</p>
@@ -19,15 +25,67 @@ const BookList = ({ books, isLoading, isError }: {
   }
 
   return (
-    <div data-test='book-list'>
-      {books.map(book => (
-        <div key={book.id} className='book-item'>
-          <h2 className='title'>{book.name}</h2>
-          <a href={`/books/${book.id}`}>View Details</a>
-        </div>
-      ))}
+    <div data-test='book-list' className={classes.root}>
+      <Grid container spacing={3}>
+        {books.map(book => (
+          <Grid item xs={4} sm={4} key={book.id} className='book-item'>
+            <Card>
+              <CardActionArea>
+                <CardContent>
+                  <Typography
+                    className={classes.name}
+                    gutterBottom
+                    variant="h5"
+                    component='h2'
+                  >
+                    {book.name}
+                  </Typography>
+                  <Typography
+                    className={classes.description}
+                    gutterBottom
+                    variant='body2'
+                    component='p'
+                  >
+                    {book.description}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Button
+                  size="small"
+                  color="primary"
+                >
+                  <Link to={`/books/${book.id}`}>View Details</Link>
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
     </div>
   )
 }
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    // padding: theme.spacing(2),
+    textAlign: 'center',
+    // color: theme.palette.text.secondary,
+  },
+  name: {
+    maxHeight: 30,
+    overflow: 'hidden',
+    textOverflow: 'elipsis',
+  },
+  description: {
+    maxHeight: 40,
+    overflow: 'hidden',
+    textOverflow: 'elipsis',
+  }
+
+}))
 
 export default BookList
